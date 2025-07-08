90e826e6427ab53e4181953d755b131de167e81f
Kenji-08 <123200723+Kenji-08@users.noreply.github.com>
2025-07-07 18:39:27 -0600

    text

--- screens/HandsScreen.tsx
+++ screens/HandsScreen.tsx
@@ -15,6 +15,10 @@ export const HandsScreen = ({ navigation }) => {
         backgroundColor="#FF3B30"
         onPress={() => navigation.goBack()}
       />
+      <Text style={styles.text}>Tactile or kinesthetic learners understand best through physical interaction, using movement and hands-on activities to engage with material. They learn by doing—touching, building, manipulating objects—and often thrive in active environments. These learners typically have strong fine motor skills and an intuitive grasp of physical tasks, such as those involved in engineering or crafts. 
+
+Their natural awareness of space and surroundings helps them process information through experience rather than abstract thinking.
+However, tactile learners may struggle with sitting still for long periods or remembering verbal instructions. To support them, it's helpful to incorporate tools like flashcards, physical models, and classroom props. Allowing movement, creative expression, or project-based learning can boost focus and retention. Field trips and interactive lessons are especially beneficial, as they let these learners connect directly with what they’re studying through real-world experience.</Text>
     </View>
   );
 };
